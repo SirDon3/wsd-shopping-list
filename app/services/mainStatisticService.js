5fa43ef72a3c36773fccf163985c79c70e9ab76f
Mohamed Shadhan <shadhanmoahmed13@gmail.com>
2024-10-12 22:33:00 +0300

    Deployment changes 5

--- app/services/mainStatisticService.js
+++ app/services/mainStatisticService.js
@@ -3,14 +3,20 @@ import { sql } from "../database/database.js";
 const findAllShoppingLists = async () => {
   const row = await sql`SELECT COUNT(*) AS total_lists FROM shopping_lists;`;
 
-  return row[0].total_lists;
+  if (row && rows.length > 0) {
+    return row[0].total_lists;
+  }
+  return false;
 };
 
 const findAllShoppingListIteams = async () => {
   const row =
     await sql`SELECT COUNT(*) AS total_lists FROM shopping_list_items;`;
 
-  return row[0].total_lists;
+  if (row && rows.length > 0) {
+    return row[0].total_lists;
+  }
+  return false;
 };
 
 export { findAllShoppingListIteams, findAllShoppingLists };
